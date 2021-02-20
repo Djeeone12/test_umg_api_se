f0055091650deac27179b82d6aaf73508e008813
Djeeone12 <60454100+Djeeone12@users.noreply.github.com>
2021-02-19 21:34:30 -0600

    Add files via upload

--- inde.js
+++ inde.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(morgan('dev'));
 // Routes to acces the root directory of the API
 app.get('/', (req, res) => {
-    res.send("<h1> Djee one </h1>");
+    res.send("<h1> Este es una tarea de Cloud computing</h1>");
 })
 
 // Route to test a json response
@@ -45,11 +45,41 @@ app.post('/invitacion', (req, res) => {
        
     })
 })
-
+// Second route for POST 
+app.post('service/action', (req, res) => {
+   
+    const END_POINT  ='https://api.linkedin.com/v2/{service}?action={actionName}'
+   // console.log(req);
+    res.send({
+      
+    });
+    axios.get(END_POINT)
+    .then(function (response) {
+      //  console.log(response.data)
+       
+    })
+})
+// 3 route for POST 
+app.post('service/{Request Body}', (req, res) => {
+   
+    const END_POINT  =' https://api.linkedin.com/v2/{service}/{Request Body}'
+   // console.log(req);
+    res.send({
+      
+    });
+    axios.get(END_POINT)
+    .then(function (response) {
+      //  console.log(response.data)
+       
+    })
+})
 
  // Testing for axios APIS
  app.get('/invitacion/{invitation Urn} ',(req,res) => {
     const END_POINT  ='https://api.linkedin.com/v2/invitations/{invitation Urn}';
+    headers: {
+  
+    }
 
     axios.get(END_POINT)
     .then(function (response) {
@@ -72,13 +102,75 @@ var config = {
   method: 'get',
   url: 'https://api.linkedin.com/v2/invitations?q=invitee&states=PENDING',
   headers: {
-      'content-Type': 'application/json',
-      'Accept' : 'application /json',
-      'Authorization':'undefided'
+      
   }
 }
 });
+app.get('/accepted',(req,res) => {
+  
+    const END_POINT  ='GET https://api.linkedin.com/v2/invitations?q=inviter&states=ACCEPTED';
+    
+var config = {
+  method: 'get',
+  url: 'GET https://api.linkedin.com/v2/invitations?q=inviter&states=ACCEPTED',
+  headers: {
+    
+        "elements": [
+          {
+            "validationToken": "gFulHMKt",
+            "created": 1485388767467,
+            "inviter": "urn:li:person:Ylpq-RobP9",
+            "id": "urn:li:invitation:6230172048157614080",
+            "state": "PENDING",
+            "lastModified": 1485388791605,
+            "sentAt": 1485388791605,
+            "message": {
+              "string": "invite_member_28"
+            },
+            "invitee": "urn:li:person:RT7VAAk2nr"
+          },
+          {
+            "validationToken": "54aKoD6Q",
+            "created": 1485379397914,
+            "inviter": "urn:li:person:Ylpq-RobP9",
+            "id": "urn:li:invitation:6230132749475291136",
+            "state": "PENDING",
+            "lastModified": 1485379412806,
+            "sentAt": 1485379412806,
+            "message": {
+              "com.linkedin.invitations.InvitationMessage": {
+                "body": "Connect with me!"
+              }
+            },
+            "invitee": "urn:li:person:r5GpfvpA3t"
+          },
+          
+        ],
+        "paging": {
+          "total": 5,
+          "count": 10,
+          "start": 0
+        }
+      
+    }
 
+}
+});
+
+app.get('/inviter/invitee ',(req,res) => {
+    const END_POINT  ='https://api.linkedin.com/v2/invitations//urn:li:invitation:123456789?projection=(created,inviter~person(firstName,lastName,headline)~emailAddress(emailAddress)~phoneAccount(phoneNumber),id,state,lastModified,sentAt,message,invitee~person(firstName,lastName,headline)~emailAddress(emailAddress)~phoneAccount(phoneNumber))';
+    
+
+    axios.get(END_POINT)
+    .then(function (response) {
+        console.log(response.data)
+        res.send(response.data)
+    })
+    .catch(function(error) {
+        console.log(error);
+        res.send(error);
+    });
+})
 // Listen Server
 app.listen(port, () => {
     console.log("Server running on port " + port);
